522f19fcd215988787641ca12c0172293774a96f
xiayesuifeng <gdigrgg@gmail.com>
2018-07-17 19:11:26 +0800

    complete login function

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,13 +1,73 @@
 import React, { Component } from 'react';
+import { withStyles } from '@material-ui/core/styles'
+import PropTypes from 'prop-types'
+import { Button, Paper, TextField } from '@material-ui/core/index.es'
+import axios from 'axios'
+
+const styles = theme => ({
+    root: {
+        display: 'flex',
+        flex: 1,
+        justifyContent: 'center',
+        flexDirection: 'column',
+        alignItems: 'center',
+        height: '100vh'
+    },
+    login: {
+        padding: theme.spacing.unit * 3
+    }
+})
 
 class Login extends Component {
-    render() {
-        return (
-            <div className="Login">
+    state = {
+        password:'',
+        message: ''
+    }
 
+    handleLogin = () => {
+        if (this.state.password === '')
+            return
+        axios.post('/api/login',{
+            password:this.state.password
+        })
+            .then(r=>{
+                if (r.data.code === 0) {
+                    this.setState({message:''})
+                    this.props.history.push('/')
+                } else if (r.data.message==='password errors'){
+                    this.setState({message:'密码错误'})
+                } else {
+                    this.setState({message:r.data.message})
+                }
+            })
+    }
+
+    render() {
+        const {classes} = this.props
+
+        return (
+            <div className={classes.root}>
+                <Paper className={classes.login}>
+                    <span>登录</span>
+                    <TextField
+                        error={this.state.message!==''}
+                        id="password"
+                        label={this.state.message!==''?this.state.message:'密码'}
+                        type="password"
+                        value={this.state.password}
+                        onChange={e => this.setState({password:e.target.value})}
+                        margin="normal"
+                        fullWidth
+                    />
+                    <Button variant="contained" color="primary" onClick={this.handleLogin}>登录</Button>
+                </Paper>
             </div>
         );
     }
 }
 
-export default Login;
+Login.propTypes = {
+    classes: PropTypes.object.isRequired,
+}
+
+export default withStyles(styles)(Login)
