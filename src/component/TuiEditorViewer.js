2cb7b93186c56f4a464a1c56615767920ad2ea38
xiayesuifeng <gdigrgg@gmail.com>
2018-08-04 23:56:04 +0800

    complete tui-editor-viewer

--- src/component/TuiEditorViewer.js
+++ src/component/TuiEditorViewer.js
@@ -4,13 +4,16 @@ import 'highlight.js/styles/github.css'
 import Viewer from 'tui-editor/dist/tui-editor-Viewer'
 
 class TuiEditorViewer extends Component {
+    constructor (props, context) {
+        super(props, context)
+        this.viewerRef = React.createRef()
+    }
 
     componentDidMount () {
         let viewer = new Viewer({
-            el: document.querySelector('#tui-editor-viewer'),
+            el: this.viewerRef.current,
             height: '100%'
         })
-
         this.setState({viewer})
     }
 
@@ -20,7 +23,7 @@ class TuiEditorViewer extends Component {
 
     render() {
         return (
-            <div id="tui-editor-viewer"/>
+            <div ref={this.viewerRef}/>
         )
     }
 }
